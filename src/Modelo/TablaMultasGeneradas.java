481b0333f1fd0b1002324cc8ed4a1983b7f288f3
CarlosChanMendez <77695013+CarlosChanMendez@users.noreply.github.com>
2021-10-16 08:45:25 -0500

    Se agregó la carpeta Modelo y los modelos

---
+++ src/Modelo/TablaMultasGeneradas.java
@@ -0,0 +1,172 @@
+package Modelo
+//created on 16-10-2021 at 8:11:22
+//class 'TablaMultasGeneradas'
+public class TablaMultasGeneradas{
+// variables for TablaMultasGeneradas
+
+	private int id;
+	private String folio;
+	private String nombres;
+	private String apellido_pat;
+	private String apellido_mat;
+	private String domicilio;
+	private String placa_vehiculo;
+	private String marca_vehiculo;
+	private String nserie_vehiculo;
+	private String modelo_vehiculo;
+	private String limite_pago;
+	private String created_by;
+	private String created_at;
+	private String updated_at;
+	private int id_multa;
+	private String KEY;
+
+
+// setters and getters for TablaMultasGeneradas
+
+// setter method for 'id'
+public void setid(int id){
+    this.id = id;
+}
+
+// getter method for 'id'
+public int getid(int id){
+    return this.id = id;
+}
+// setter method for 'folio'
+public void setfolio(String folio){
+    this.folio = folio;
+}
+
+// getter method for 'folio'
+public String getfolio(String folio){
+    return this.folio = folio;
+}
+// setter method for 'nombres'
+public void setnombres(String nombres){
+    this.nombres = nombres;
+}
+
+// getter method for 'nombres'
+public String getnombres(String nombres){
+    return this.nombres = nombres;
+}
+// setter method for 'apellido_pat'
+public void setapellido_pat(String apellido_pat){
+    this.apellido_pat = apellido_pat;
+}
+
+// getter method for 'apellido_pat'
+public String getapellido_pat(String apellido_pat){
+    return this.apellido_pat = apellido_pat;
+}
+// setter method for 'apellido_mat'
+public void setapellido_mat(String apellido_mat){
+    this.apellido_mat = apellido_mat;
+}
+
+// getter method for 'apellido_mat'
+public String getapellido_mat(String apellido_mat){
+    return this.apellido_mat = apellido_mat;
+}
+// setter method for 'domicilio'
+public void setdomicilio(String domicilio){
+    this.domicilio = domicilio;
+}
+
+// getter method for 'domicilio'
+public String getdomicilio(String domicilio){
+    return this.domicilio = domicilio;
+}
+// setter method for 'placa_vehiculo'
+public void setplaca_vehiculo(String placa_vehiculo){
+    this.placa_vehiculo = placa_vehiculo;
+}
+
+// getter method for 'placa_vehiculo'
+public String getplaca_vehiculo(String placa_vehiculo){
+    return this.placa_vehiculo = placa_vehiculo;
+}
+// setter method for 'marca_vehiculo'
+public void setmarca_vehiculo(String marca_vehiculo){
+    this.marca_vehiculo = marca_vehiculo;
+}
+
+// getter method for 'marca_vehiculo'
+public String getmarca_vehiculo(String marca_vehiculo){
+    return this.marca_vehiculo = marca_vehiculo;
+}
+// setter method for 'nserie_vehiculo'
+public void setnserie_vehiculo(String nserie_vehiculo){
+    this.nserie_vehiculo = nserie_vehiculo;
+}
+
+// getter method for 'nserie_vehiculo'
+public String getnserie_vehiculo(String nserie_vehiculo){
+    return this.nserie_vehiculo = nserie_vehiculo;
+}
+// setter method for 'modelo_vehiculo'
+public void setmodelo_vehiculo(String modelo_vehiculo){
+    this.modelo_vehiculo = modelo_vehiculo;
+}
+
+// getter method for 'modelo_vehiculo'
+public String getmodelo_vehiculo(String modelo_vehiculo){
+    return this.modelo_vehiculo = modelo_vehiculo;
+}
+// setter method for 'limite_pago'
+public void setlimite_pago(String limite_pago){
+    this.limite_pago = limite_pago;
+}
+
+// getter method for 'limite_pago'
+public String getlimite_pago(String limite_pago){
+    return this.limite_pago = limite_pago;
+}
+// setter method for 'created_by'
+public void setcreated_by(String created_by){
+    this.created_by = created_by;
+}
+
+// getter method for 'created_by'
+public String getcreated_by(String created_by){
+    return this.created_by = created_by;
+}
+// setter method for 'created_at'
+public void setcreated_at(String created_at){
+    this.created_at = created_at;
+}
+
+// getter method for 'created_at'
+public String getcreated_at(String created_at){
+    return this.created_at = created_at;
+}
+// setter method for 'updated_at'
+public void setupdated_at(String updated_at){
+    this.updated_at = updated_at;
+}
+
+// getter method for 'updated_at'
+public String getupdated_at(String updated_at){
+    return this.updated_at = updated_at;
+}
+// setter method for 'id_multa'
+public void setid_multa(int id_multa){
+    this.id_multa = id_multa;
+}
+
+// getter method for 'id_multa'
+public int getid_multa(int id_multa){
+    return this.id_multa = id_multa;
+}
+// setter method for 'KEY'
+public void setKEY(String KEY){
+    this.KEY = KEY;
+}
+
+// getter method for 'KEY'
+public String getKEY(String KEY){
+    return this.KEY = KEY;
+}
+
+}
---
+++ src/Modelo/TablaPropietario.java
@@ -0,0 +1,152 @@
+package Modelo
+//created on 16-10-2021 at 8:11:22
+//class 'TablaPropietario'
+public class TablaPropietario{
+// variables for TablaPropietario
+
+	private int id;
+	private String nombres;
+	private String apellido_pat;
+	private String apellido_mat;
+	private String sexo;
+	private String edad;
+	private String domicilio;
+	private String lugar;
+	private String created_by;
+	private String updated_by;
+	private String created_at;
+	private String updated_at;
+	private int id_vehiculo;
+	private String KEY;
+
+
+// setters and getters for TablaPropietario
+
+// setter method for 'id'
+public void setid(int id){
+    this.id = id;
+}
+
+// getter method for 'id'
+public int getid(int id){
+    return this.id = id;
+}
+// setter method for 'nombres'
+public void setnombres(String nombres){
+    this.nombres = nombres;
+}
+
+// getter method for 'nombres'
+public String getnombres(String nombres){
+    return this.nombres = nombres;
+}
+// setter method for 'apellido_pat'
+public void setapellido_pat(String apellido_pat){
+    this.apellido_pat = apellido_pat;
+}
+
+// getter method for 'apellido_pat'
+public String getapellido_pat(String apellido_pat){
+    return this.apellido_pat = apellido_pat;
+}
+// setter method for 'apellido_mat'
+public void setapellido_mat(String apellido_mat){
+    this.apellido_mat = apellido_mat;
+}
+
+// getter method for 'apellido_mat'
+public String getapellido_mat(String apellido_mat){
+    return this.apellido_mat = apellido_mat;
+}
+// setter method for 'sexo'
+public void setsexo(String sexo){
+    this.sexo = sexo;
+}
+
+// getter method for 'sexo'
+public String getsexo(String sexo){
+    return this.sexo = sexo;
+}
+// setter method for 'edad'
+public void setedad(String edad){
+    this.edad = edad;
+}
+
+// getter method for 'edad'
+public String getedad(String edad){
+    return this.edad = edad;
+}
+// setter method for 'domicilio'
+public void setdomicilio(String domicilio){
+    this.domicilio = domicilio;
+}
+
+// getter method for 'domicilio'
+public String getdomicilio(String domicilio){
+    return this.domicilio = domicilio;
+}
+// setter method for 'lugar'
+public void setlugar(String lugar){
+    this.lugar = lugar;
+}
+
+// getter method for 'lugar'
+public String getlugar(String lugar){
+    return this.lugar = lugar;
+}
+// setter method for 'created_by'
+public void setcreated_by(String created_by){
+    this.created_by = created_by;
+}
+
+// getter method for 'created_by'
+public String getcreated_by(String created_by){
+    return this.created_by = created_by;
+}
+// setter method for 'updated_by'
+public void setupdated_by(String updated_by){
+    this.updated_by = updated_by;
+}
+
+// getter method for 'updated_by'
+public String getupdated_by(String updated_by){
+    return this.updated_by = updated_by;
+}
+// setter method for 'created_at'
+public void setcreated_at(String created_at){
+    this.created_at = created_at;
+}
+
+// getter method for 'created_at'
+public String getcreated_at(String created_at){
+    return this.created_at = created_at;
+}
+// setter method for 'updated_at'
+public void setupdated_at(String updated_at){
+    this.updated_at = updated_at;
+}
+
+// getter method for 'updated_at'
+public String getupdated_at(String updated_at){
+    return this.updated_at = updated_at;
+}
+// setter method for 'id_vehiculo'
+public void setid_vehiculo(int id_vehiculo){
+    this.id_vehiculo = id_vehiculo;
+}
+
+// getter method for 'id_vehiculo'
+public int getid_vehiculo(int id_vehiculo){
+    return this.id_vehiculo = id_vehiculo;
+}
+// setter method for 'KEY'
+public void setKEY(String KEY){
+    this.KEY = KEY;
+}
+
+// getter method for 'KEY'
+public String getKEY(String KEY){
+    return this.KEY = KEY;
+}
+
+}
