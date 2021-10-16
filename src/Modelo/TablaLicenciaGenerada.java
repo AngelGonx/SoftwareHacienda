b573457590ecff5048fbea8d9e49ba60883bd5b8
Carlos Mendez <carloschan.mendez@gmail.com>
2021-10-16 08:19:50 -0500

    Se Agregó la carpeta Modelo y las clases correspondientes

---
+++ src/Modelo/TablaLicenciaGenerada.java
@@ -0,0 +1,162 @@
+package Modelo
+//created on 16-10-2021 at 8:11:22
+//class 'TablaLicenciaGenerada'
+public class TablaLicenciaGenerada{
+// variables for TablaLicenciaGenerada
+
+	private int id;
+	private String nombres;
+	private String apellido_pat;
+	private String apellido_mat;
+	private String edad;
+	private String domicilio;
+	private String fecha_nac;
+	private String cp;
+	private String lugar;
+	private String celular;
+	private String crated_by;
+	private String created_at;
+	private String updated_at;
+	private int id_tipo_licencia;
+	private String KEY;
+
+
+// setters and getters for TablaLicenciaGenerada
+
+// setter method for 'id'
+public void setid(int id){
+    this.id = id;
+}
+
+// getter method for 'id'
+public int getid(int id){
+    return this.id = id;
+}
+// setter method for 'nombres'
+public void setnombres(String nombres){
+    this.nombres = nombres;
+}
+
+// getter method for 'nombres'
+public String getnombres(String nombres){
+    return this.nombres = nombres;
+}
+// setter method for 'apellido_pat'
+public void setapellido_pat(String apellido_pat){
+    this.apellido_pat = apellido_pat;
+}
+
+// getter method for 'apellido_pat'
+public String getapellido_pat(String apellido_pat){
+    return this.apellido_pat = apellido_pat;
+}
+// setter method for 'apellido_mat'
+public void setapellido_mat(String apellido_mat){
+    this.apellido_mat = apellido_mat;
+}
+
+// getter method for 'apellido_mat'
+public String getapellido_mat(String apellido_mat){
+    return this.apellido_mat = apellido_mat;
+}
+// setter method for 'edad'
+public void setedad(String edad){
+    this.edad = edad;
+}
+
+// getter method for 'edad'
+public String getedad(String edad){
+    return this.edad = edad;
+}
+// setter method for 'domicilio'
+public void setdomicilio(String domicilio){
+    this.domicilio = domicilio;
+}
+
+// getter method for 'domicilio'
+public String getdomicilio(String domicilio){
+    return this.domicilio = domicilio;
+}
+// setter method for 'fecha_nac'
+public void setfecha_nac(String fecha_nac){
+    this.fecha_nac = fecha_nac;
+}
+
+// getter method for 'fecha_nac'
+public String getfecha_nac(String fecha_nac){
+    return this.fecha_nac = fecha_nac;
+}
+// setter method for 'cp'
+public void setcp(String cp){
+    this.cp = cp;
+}
+
+// getter method for 'cp'
+public String getcp(String cp){
+    return this.cp = cp;
+}
+// setter method for 'lugar'
+public void setlugar(String lugar){
+    this.lugar = lugar;
+}
+
+// getter method for 'lugar'
+public String getlugar(String lugar){
+    return this.lugar = lugar;
+}
+// setter method for 'celular'
+public void setcelular(String celular){
+    this.celular = celular;
+}
+
+// getter method for 'celular'
+public String getcelular(String celular){
+    return this.celular = celular;
+}
+// setter method for 'crated_by'
+public void setcrated_by(String crated_by){
+    this.crated_by = crated_by;
+}
+
+// getter method for 'crated_by'
+public String getcrated_by(String crated_by){
+    return this.crated_by = crated_by;
+}
+// setter method for 'created_at'
+public void setcreated_at(String created_at){
+    this.created_at = created_at;
+}
+
+// getter method for 'created_at'
+public String getcreated_at(String created_at){
+    return this.created_at = created_at;
+}
+// setter method for 'updated_at'
+public void setupdated_at(String updated_at){
+    this.updated_at = updated_at;
+}
+
+// getter method for 'updated_at'
+public String getupdated_at(String updated_at){
+    return this.updated_at = updated_at;
+}
+// setter method for 'id_tipo_licencia'
+public void setid_tipo_licencia(int id_tipo_licencia){
+    this.id_tipo_licencia = id_tipo_licencia;
+}
+
+// getter method for 'id_tipo_licencia'
+public int getid_tipo_licencia(int id_tipo_licencia){
+    return this.id_tipo_licencia = id_tipo_licencia;
+}
+// setter method for 'KEY'
+public void setKEY(String KEY){
+    this.KEY = KEY;
+}
+
+// getter method for 'KEY'
+public String getKEY(String KEY){
+    return this.KEY = KEY;
+}
+
+}
